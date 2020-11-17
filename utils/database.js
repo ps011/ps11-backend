d75ebdaa4ed48b7e7c76f8a23a51aa96a846b715
Prasheel Soni <prasheelsoni11@gmail.com>
2020-11-17 13:27:48 +0530

    refactor(database): update DB creds

--- utils/database.js
+++ utils/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const url = `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@ds215633.mlab.com:15633/portfolio`;
-
+// const url = `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@ds215633.mlab.com:15633/portfolio`;
+const url = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@portfolio.ddmdn.mongodb.net/portfolio?retryWrites=true&w=majority`
 module.exports = {
 
     connectToServer: async () => {
